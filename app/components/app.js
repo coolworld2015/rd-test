d4d4f389899fcf356a71d191600ba4f7b08a4611
coolworld2015 <wintermute2015@ukr.net>
2016-09-21 16:16:24 +0300

    [general] - refactoring

--- app/components/app.js
+++ app/components/app.js
@@ -1,81 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './../css/style.css';
 
 import ListItem from './listItem';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-			
-		this.state = {
-			name: 'CoolWorld',
-			clients: []
-		}	
-	}
-  
-	componentDidMount() {
-		this.getClients();
-	}
-	
-	getClients() {
-		fetch('http://ui-warehouse.herokuapp.com/api/clients/get')
-		.then((response)=> response.json())
-		.then((responseData)=> {
-			this.setState({
-				clients:  responseData.sort(this.sort),
-				resultsCount: responseData.length
-			});
-		})
-		.catch((error)=> {
-			this.setState({
-				serverError: true
-			});
-		}) 
-	}
-	
-	sort(a, b) {
-		var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
-		if (nameA < nameB) {
-			return -1
-		}
-		if (nameA > nameB) {
-			return 1
-		}
-		return 0;
-	}
+    constructor(props) {
+        super(props);
 
-	changeHandler(event) {
-		this.setState({
-			name: event.target.value
-		})	
-	}
-	
-	showClients() {
-		return this.state.clients.map(function(item) {
-			return <ListItem id={item.id} name={item.name} key={item.id} />
-		})
-	}
-	
-	render() {
-		return (
-		  <div>
-			<div className="header">{this.state.name}</div>
-			<hr />
-			<input onChange={(event) => {
-					this.setState({
-						name: event.target.value
-					})	
-			}} />
-			<hr />
-			
-			<div className="header">Clients ({this.state.resultsCount})</div>
-			<hr />
+        this.state = {
+            name: 'CoolWorld',
+            clients: []
+        }
+    }
 
-			{this.showClients()}
+    componentDidMount() {
+        this.getClients();
+    }
 
-		 </div>
-		)
-	}
+    getClients() {
+        fetch('http://ui-warehouse.herokuapp.com/api/clients/get')
+            .then((response)=> response.json())
+            .then((responseData)=> {
+                this.setState({
+                    clients: responseData.sort(this.sort),
+                    resultsCount: responseData.length
+                });
+            })
+            .catch((error)=> {
+                this.setState({
+                    serverError: true
+                });
+            })
+    }
+
+    sort(a, b) {
+        var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+        if (nameA < nameB) {
+            return -1
+        }
+        if (nameA > nameB) {
+            return 1
+        }
+        return 0;
+    }
+
+    changeHandler(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    showClients() {
+        return this.state.clients.map(function (item) {
+            return <ListItem id={item.id} name={item.name} key={item.id}/>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="header">{this.state.name}</div>
+                <hr />
+                <input onChange={(event) => {
+                    this.setState({
+                        name: event.target.value
+                    })
+                }}/>
+                <hr />
+
+                <div className="header">Clients ({this.state.resultsCount})</div>
+                <hr />
+
+                {this.showClients()}
+
+            </div>
+        )
+    }
 }
 
 module.exports = App; 
--- app/components/listItem.js
+++ app/components/listItem.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class ListItem extends Component {
-  constructor(props) {
-	super(props);
+    constructor(props) {
+        super(props);
 
-	this.state = {
- 
-	}	
-  }
-	render() {
-		return (
-			<div>
-				<h4>{this.props.id} - {this.props.name}</h4>
-				<hr/>
-			</div>
-		);
-	}
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>{this.props.id} - {this.props.name}</h4>
+                <hr/>
+            </div>
+        );
+    }
 }
 
 module.exports = ListItem;
