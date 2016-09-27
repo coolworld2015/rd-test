79884f4e0472ca90d35eb30786e003d9ffb6117d
coolworld2015 <wintermute2015@ukr.net>
2016-09-27 15:29:56 +0300

    [redux] - added redux

--- app/index.js
+++ app/index.js
@@ -6,8 +6,38 @@ import routes from './routes';
 import './index.html';
 import './css/style.css';
 
-//ReactDOM.render(<App />, document.getElementById('container'));
+//ReactDOM.render(<App />, document.getElementById('container')); //TODO: Without Router
+
 render(
     <Router history={hashHistory} routes={routes}/>,
     document.getElementById('app')
 );
+
+const store = Redux.createStore(function (state, action) {
+    switch (action.type) {
+        case 'ADD_CARD':
+            let newCard = Object.assign({}, action.data, {
+                score: 1,
+                id: +new Date
+            });
+
+            return Object.assign({}, state, {
+               cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+            });
+
+        default:
+            return state || {};
+    }
+});
+
+store.subscribe(() => {
+   console.log(store.getState());
+});
+
+store.dispatch({
+   type: 'ADD_CARD',
+    data: {
+       front: 'front',
+       back: 'back'
+    }
+});
